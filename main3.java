f34214e2eb6aede3c8598fc9e2fcef4e2b5bc168
Darika-M <darikaratha96@gmail.com>
2026-01-31 19:20:35 +0530

    Modified main3.java

--- main3.java
+++ main3.java
@@ -1,5 +1,26 @@
-package java_code;
+class SwapRows {
+    public static void main(String[] args) {
 
-public class main3 {
-    
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = a.length;
+        int cols = a[0].length;
+
+      
+        for (int j = 0; j < cols; j++) {
+            int temp = a[0][j];
+            a[0][j] = a[rows - 1][j];
+            a[rows - 1][j] = temp;
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
